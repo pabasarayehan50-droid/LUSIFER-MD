00c3b47b6fb14c4a118fdc52bd252c585e4f2eaa
LUCIFER-X11 <scamerme22@gmail.com>
2025-08-16 05:56:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+/*─────────────────────────────────────────────────────────────
+ 🌟 OWNER     : LUCIFER  
+ 🌐 Supported : GITHUB  
+ 🔒 CODE PROTECT  
+ 📺 YT        : https://youtube.com/@demoneye_official  
+ 💬 TG        : https://t.me/Next_DYS  
+ 💻 GITHUB    : https://github.com/DemonEye143  
+─────────────────────────────────────────────────────────────*/
+
+
+require('dotenv').config();
+
+global.APIs = {
+    xteam: 'https://api.xteam.xyz',
+    dzx: 'https://api.dhamzxploit.my.id',
+    lol: 'https://api.lolhuman.xyz',
+    violetics: 'https://violetics.pw',
+    neoxr: 'https://api.neoxr.my.id',
+    zenzapis: 'https://zenzapis.xyz',
+    akuari: 'https://api.akuari.my.id',
+    akuari2: 'https://apimu.my.id',
+    nrtm: 'https://fg-nrtm.ddns.net',
+    bg: 'http://bochil.ddns.net',
+    fgmods: 'https://api-fgmods.ddns.net'
+};
+
+global.APIKeys = {
+    'https://api.xteam.xyz': 'd90a9e986e18778b',
+    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+    'https://api.neoxr.my.id': 'yourkey',
+    'https://violetics.pw': 'beta',
+    'https://zenzapis.xyz': 'yourkey',
+    'https://api-fgmods.ddns.net': 'fg-dylux'
+};
+
+module.exports = {
+    WARN_COUNT: 3,
+    APIs: global.APIs,
+    APIKeys: global.APIKeys
+};
